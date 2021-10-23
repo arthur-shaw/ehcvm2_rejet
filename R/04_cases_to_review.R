940849d2e4be0a0eacb61ff4670e3054d8282ad1
Arthur Shaw <47256431+arthur-shaw@users.noreply.github.com>
2021-10-22 22:08:45 -0400

    Adapter l'identification d'entretiens à traiter et chargement de données au besoins d'EHCVM

--- R/04_cases_to_review.R
+++ R/04_cases_to_review.R
@@ -3,7 +3,7 @@
 # =============================================================================
 
 objects_needed <- c(
-    "hholds"
+    "combined_dir"
 )
 
 check_exists(objects_needed)
@@ -13,29 +13,62 @@ check_exists(objects_needed)
 # =============================================================================
 
 # create cases to review overall
-cases_to_review <- hholds %>%
-    # complete based on SuSo status
+cases_to_review <- haven::read_dta(file = paste0(combined_dir, "EHCVM2_MENAGE_VAGUE1_BFA.dta")) %>%
+    # selon le statut SuSo
     dplyr::filter(interview__status %in% statuses_to_reject) %>%
     # selon les données de l'entretien
     dplyr::filter(
-        dplyr::if_any(
-            .cols = c(
-                complete_aas, complete_unps, 
-                complete_single_phase, complete_full_interview
-            ),
-            .fns = ~ .x == 1
-        )
+        # résultat: achevé
+        (s00q08 %in% c(1, 2))
+        & 
+        # toutes les visites faites
+        (visite1 == 1 & visite2 == 2 & visite3 == 3)
     ) %>%
-    dplyr::mutate(interview__complete = 1) %>%
-    dplyr::select(
-        interview__id, interview__key, interview__status,
-        complete_aas, complete_unps, complete_single_phase, complete_full_interview,
-        interview__complete
-    )
+    dplyr::select(interview__id, interview__key)
 
-# create sets for filtering attributes
-cases_aas <- dplyr::filter(cases_to_review, complete_aas == 1)
-cases_unps <- dplyr::filter(cases_to_review, complete_unps == 1)
-cases_unoma <- dplyr::filter(cases_to_review, complete_unoma == 1)
-cases_single_phase <- dplyr::filter(cases_to_review, complete_single_phase == 1)
-cases_full_interview <- dplyr::filter(cases_to_review, complete_full_interview == 1)
+# =============================================================================
+# Charger les données requises
+# =============================================================================
+
+load_filtered <- function(
+    dir,
+    file,
+    name = gsub(pattern = "\\.dta", replacement = "", x = file),
+    filter_df
+) {
+    df <- haven::read_dta(file = paste0(dir, file))
+    
+    df_filtered <- df %>%
+        dplyr::semi_join(filter_df, by = c("interview__id", "interview__key"))
+    
+    assign(
+        x = name,
+        value = df_filtered,
+        envir = .GlobalEnv
+    )
+    
+}
+
+fichiers <- c("EHCVM2_MENAGE_VAGUE1_BFA.dta", "membres.dta", "filets_securite.dta", "equipements.dta", "interview__errors.dta")
+fichier_noms <- c("menages", "membres", "filets_securite", "equipements", "suso_errors")
+
+purrr::walk2(
+    .x = fichiers, 
+    .y = fichier_noms,
+    .f = ~ load_filtered(
+        dir = combined_dir,
+        file = .x,
+        name = .y,
+        filter_df = cases_to_review
+    )
+)
+
+# TODO: uncomment once calorie file exists
+# purrr::walk(
+#     .x = "calories_totales.dta", "calories_par_item.dta",
+#     .f = ~ load_filtered(
+#         dir = derived_dir,
+#         file = .x,
+#         filter_df = cases_to_review
+#     )
+# )
