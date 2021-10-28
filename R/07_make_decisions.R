aa44f9f50714211b99c5db2cfbd8f5c23a153bf3
Arthur Shaw <47256431+arthur-shaw@users.noreply.github.com>
2021-10-27 21:52:22 -0400

    Créer des bases pour capter les décisions
    Deux à trois groupes de bases par décision:
    
    - Les identifiants des entretiens
    - Les problèmes identifiés pour ces entretiens
    - Les informations à passer à l'API pour exécuter l'action

--- R/07_make_decisions.R
+++ R/07_make_decisions.R
@@ -19,7 +19,7 @@ check_exists(objects_needed)
 # returns a data frame of cases that contain comments
 interviews_with_comments <- susoreview::check_for_comments(
     df_comments = comments, 
-    df_issues = issues, 
+    df_issues = issues_plus_miss_and_suso, 
     df_cases_to_review = cases_to_review
 )
 
@@ -28,6 +28,7 @@ interviews_with_comments <- susoreview::check_for_comments(
 decisions <- susoreview::decide_action(
     df_cases_to_review = cases_to_review,
     df_issues = issues_plus_miss_and_suso,
+    issue_types_to_reject = statuses_to_reject,
     df_has_comments = interviews_with_comments,
     df_interview_stats = interview_stats
 )
@@ -45,3 +46,53 @@ revised_decisions <- susoreview::flag_persistent_issues(
     df_comments = comments,
     df_to_reject = to_reject
 )
+
+# =============================================================================
+# Extract decisions into data representing them
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+# To reject
+# -----------------------------------------------------------------------------
+
+to_reject_ids <- revised_decisions[["to_reject"]] %>%
+    dplyr::select(interview__id) %>%
+    dplyr::left_join(cases_to_review, by = "interview__id")
+
+to_reject_issues <- to_reject_ids %>%
+    dplyr::left_join(issues_plus_miss_and_suso)
+
+to_reject_api <- revised_decisions[["to_reject"]]
+
+# -----------------------------------------------------------------------------
+# To review
+# -----------------------------------------------------------------------------
+
+to_review_ids <- decisions[["to_review"]]
+
+to_review_issues <- to_review_ids %>%
+    dplyr::left_join(
+        issues_plus_miss_and_suso, 
+        by = c("interview__id", "interview__key")
+    )
+
+# -----------------------------------------------------------------------------
+# To follow up
+# -----------------------------------------------------------------------------
+
+to_follow_up_ids <- revised_decisions[["to_follow_up"]] %>%
+    dplyr::left_join(cases_to_review, by = "interview__id") %>%
+    dplyr::select(interview__id, interview__key)
+
+to_follow_up_issues <- revised_decisions[["to_follow_up"]] %>%
+    dplyr::left_join(issues_plus_miss_and_suso, by = "interview__id") %>%
+    dplyr::left_join(
+        cases_to_review, 
+        by = c("interview__id", "interview__key")
+    ) %>%
+    dplyr::select(
+        interview__id, interview__key, interview__status,
+        dplyr::starts_with("issue_")
+    )
+
+to_follow_up_api <- revised_decisions[["to_follow_up"]]
