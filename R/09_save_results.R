25ff885c8a4f1d6232059d7e24eeb045d3ebc393
Arthur Shaw <47256431+arthur-shaw@users.noreply.github.com>
2021-10-27 21:58:55 -0400

    Sauvegarder les résultats dans sur disque en format Excel et Stata
    Pour ce faire:
    
    - Installer un package pour écrire vers Excel
    - Concevoir une fonction pour sauvegarder et l'exécuter
    - Ignorer ces sorties dans le contrôle de version

--- R/09_save_results.R
+++ R/09_save_results.R
@@ -1,8 +1,79 @@
-# issues
+# =============================================================================
+# Check that necessary objects exist
+# =============================================================================
 
-# to reject
+objects_needed <- c(
+    "issues_plus_miss_and_suso",
+    # to reject
+    "to_reject_ids",
+    "to_reject_issues",
+    "to_reject_api",
+    # to review
+    "to_review_ids",
+    "to_review_issues",
+    "to_review_api"
+    # to follow up
 
-# to review
+)
 
-# to follow up
+check_exists(objects_needed)
+
+# =============================================================================
+# Write decisions to disk in Excel and Stata format
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+# Define function
+# -----------------------------------------------------------------------------
+
+write_to_excel_and_stata <- function(
+    data,
+    dir,
+    name = deparse(substitute(data))
+) {
+
+    # Excel
+    writexl::write_xlsx(x = data, path = paste0(dir, name, ".xlsx"), col_names = TRUE)
+
+    # Stata
+    haven::write_dta(data = data, path = paste0(dir, name, ".dta"))
+
+}
+
+# -----------------------------------------------------------------------------
+# Issues
+# -----------------------------------------------------------------------------
+
+# data
+write_to_excel_and_stata(data = issues_plus_miss_and_suso, dir = output_dir)
+
+# -----------------------------------------------------------------------------
+# To reject
+# -----------------------------------------------------------------------------
+
+purrr::walk2(
+    .x = list(to_reject_ids, to_reject_issues, to_reject_api),
+    .y = c("to_reject_ids", "to_reject_issues", "to_reject_api"),
+    .f = ~ write_to_excel_and_stata(.x, dir = output_dir, name = .y)
+)
+
+# -----------------------------------------------------------------------------
+# To review
+# -----------------------------------------------------------------------------
+
+purrr::walk2(
+    .x = list(to_review_ids, to_review_issues, to_review_api),
+    .y = c("to_review_ids", "to_review_issues", "to_review_api"),
+    .f = ~ write_to_excel_and_stata(.x, dir = output_dir, name = .y)
+)
+
+# -----------------------------------------------------------------------------
+# To follow up
+# -----------------------------------------------------------------------------
+
+purrr::walk2(
+    .x = list(to_follow_up_ids, to_follow_up_issues, to_follow_up_api),
+    .y = c("to_follow_up_ids", "to_follow_up_issues", "to_follow_up_api"),
+    .f = ~ write_to_excel_and_stata(.x, dir = output_dir, name = .y)
+)
 
