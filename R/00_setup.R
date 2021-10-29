8844cd6b9f1d4b6ee6e8ec9a244781107695027b
Arthur Shaw <47256431+arthur-shaw@users.noreply.github.com>
2021-10-29 14:41:53 -0400

    Supprimer les informations périmées--entrées et sorties--comme préparatif

--- R/00_setup.R
+++ R/00_setup.R
@@ -14,11 +14,35 @@ output_dir <- paste0(proj_dir, "output/")    # /output/
 
 
 # =============================================================================
-# Purge past data
+# Purge stale data
 # =============================================================================
 
 # remove zip files
-
+zips_to_delete <- fs::dir_ls(
+    path = download_dir, 
+    recurse = FALSE, 
+    type = "file", 
+    regexp = "\\.zip$"
+)
+fs::file_delete(zips_to_delete)
 
 # remove unzipped folders and the data they contain
+dirs_to_delete <- fs::dir_ls(
+    path = download_dir, 
+    recurse = FALSE, 
+    type = "directory"
+)
+fs::dir_delete(dirs_to_delete)
 
+# =============================================================================
+# Purge stale outputs
+# =============================================================================
+
+# remove Excel and Stata files
+results_to_delete <- fs::dir_ls(
+    path = output_dir, 
+    recurse = FALSE, 
+    type = "file", 
+    regexp = "\\.xlsx$|\\.dta$"
+)
+fs::file_delete(results_to_delete)
